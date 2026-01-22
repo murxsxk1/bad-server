76aff3a075aae24937fe9ade16c159de0864c273
Danila <novash.danila@yandex.by>
2026-01-22 11:31:38 +0300

    fix: fix tests errors

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -3,26 +3,35 @@ import { constants } from 'http2'
 import BadRequestError from '../errors/bad-request-error'
 
 export const uploadFile = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  if (!req.file) {
-    return next(new BadRequestError('Файл не загружен'))
-  }
+    if (!req.file) {
+        return next(new BadRequestError('Файл не загружен'))
+    }
 
-  try {
-    const fileName = process.env.UPLOAD_PATH
-      ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
-      : `/${req.file?.filename}`
+    // Добавлено: проверка минимального размера файла (больше 2KB)
+    if (req.file.size <= 2048) {
+        return next(
+            new BadRequestError(
+                'Файл слишком маленький (должен быть больше 2KB)'
+            )
+        )
+    }
 
-    return res.status(constants.HTTP_STATUS_CREATED).send({
-      fileName,
-      originalName: req.file?.originalname,
-    })
-  } catch (error) {
-    return next(error)
-  }
+    try {
+        const fileName = process.env.UPLOAD_PATH
+            ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
+            : `/${req.file?.filename}`
+
+        return res.status(constants.HTTP_STATUS_CREATED).send({
+            fileName,
+            originalName: req.file?.originalname,
+        })
+    } catch (error) {
+        return next(error)
+    }
 }
 
 export default {}
